19e9e8ed6314f1b30bbdaf7dec0ea8b85b95a1ab
sakibhusainzaidi2000@gmail.com <sakibhusainzaidi2000@gmail.com>
2025-01-08 12:42:14 +0530

    bcryptjs has installed

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,5 @@
 const User = require('../models/user.models'); 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 module.exports.signup = async (req, res) => {
