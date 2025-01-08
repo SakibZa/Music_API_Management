f1950aab7833f982aa0aa5af2db770fd45574c5e
sakibhusainzaidi2000@gmail.com <sakibhusainzaidi2000@gmail.com>
2025-01-08 13:59:02 +0530

    script has been added

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}); 
+mongoose.connect(process.env.MONGODB_URL); 
 
 mongoose.connection.on('connected', () => {
     console.log('Connected to MongoDB');
